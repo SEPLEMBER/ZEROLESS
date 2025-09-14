21bebb0f1fcdb505512856fdfc7524bfbd14711c
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-09-14 16:16:56 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,53 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace 'com.nemesis.droidcrypt'
-    compileSdk 34
+    namespace = "com.nemesis.droidcrypt"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.nemesis.droidcrypt"
-        minSdk 26
-        targetSdk 34
-        versionCode 2
-        versionName "1.1"
+        applicationId = "com.nemesis.droidcrypt"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.1"
 
-        multiDexEnabled true
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
-            signingConfig null // Убрана подпись для неподписанного APK
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = null // Убрана подпись для неподписанного APK
         }
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 
     dependenciesInfo {
-        includeInApk false
-        includeInBundle false
+        includeInApk = false
+        includeInBundle = false
     }
 }
 
 dependencies {
-    implementation libs.appcompat
-    implementation libs.material
-    implementation libs.bcprov.jdk15on
-    implementation 'androidx.multidex:multidex:2.0.1'
-    implementation 'androidx.recyclerview:recyclerview:1.3.2'
-    testImplementation libs.junit
-    androidTestImplementation libs.ext.junit
-    androidTestImplementation libs.espresso.core
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.bcprov.jdk15on)
+    implementation(libs.multidex)
+    implementation(libs.recyclerview)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
