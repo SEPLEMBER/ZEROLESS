861634d4f792e48b277b29b51db074e70576a57f
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-09-14 15:45:22 +0700

    Update and rename ExampleInstrumentedTest.java to ExampleInstrumentedTest.kt

---
+++ app/src/androidTest/java/com/example/droidcrypt/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.nemesis.droidcrypt
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.test.assertEquals
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.nemesis.droidcrypt", appContext.packageName)
+    }
+}
