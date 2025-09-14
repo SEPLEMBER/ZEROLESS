0cadc17ea6f75779b7cd43db88c05a60331923df
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-09-14 18:35:32 +0700

    Update ExampleUnitTest.kt

--- app/src/test/kotlin/com/nemesis/droidcrypt/ExampleUnitTest.kt
+++ app/src/test/kotlin/com/nemesis/droidcrypt/ExampleUnitTest.kt
@@ -1,13 +1,8 @@
 package com.nemesis.droidcrypt
 
 import org.junit.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
