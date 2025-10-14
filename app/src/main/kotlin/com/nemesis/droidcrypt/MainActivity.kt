faa072115d00e1100de7e7efe315793ac2595bd6
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-10-14 22:03:04 +0700

    Update MainActivity.kt

--- app/src/main/kotlin/com/nemesis/droidcrypt/MainActivity.kt
+++ app/src/main/kotlin/com/nemesis/droidcrypt/MainActivity.kt
@@ -63,7 +63,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         setupText.setOnClickListener {
-            val i = Intent(this@MainActivity, TemplateActivity::class.java)
+            val i = Intent(this@MainActivity, ArrivalActivity::class.java)
             startActivity(i)
         }
 
